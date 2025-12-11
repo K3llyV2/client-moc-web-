974f745c4a4f531d7cdbd001551557e030a7978c
unknown <kellymemzy@gmail.com>
2025-12-11 19:03:36 +0300

    resume window implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Navbar, Welcome, Dock} from "#Components";
-import {Safari, Terminal} from "#windows"
+import {Resume, Safari, Terminal} from "#windows"
 
 import gsap from "gsap";
 import {Draggable} from 'gsap/Draggable';
@@ -16,6 +16,7 @@ const App = () => {
 
           <Terminal/>
           <Safari/>
+          <Resume/>
       </main>
     )
 }
